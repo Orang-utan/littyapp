a745d917ac696a96cd3c1ad08aed725bb34ef2b8
Daniel Tian <dtian.95@gmail.com>
2020-05-09 16:01:30 +0800

    created account branch

--- littyapp/frontend/src/components/Navbar.js
+++ littyapp/frontend/src/components/Navbar.js
@@ -10,21 +10,15 @@ const Navbar = () => {
 
   return (
     <div class="ui menu">
-      <div class="header item">
-        <Link to="/" style={style.item}>
-          Litty App
-        </Link>
-      </div>
-      <a class="item">
-        <Link to="/login" style={style.item}>
-          Login
-        </Link>
-      </a>
-      <a class="item">
-        <Link to="/register" style={style.item}>
-          Register
-        </Link>
-      </a>
+      <Link to="/" style={style.item}>
+        <div class="header item">Litty App</div>
+      </Link>
+      <Link to="/login" style={style.item}>
+        <a class="item">Login</a>
+      </Link>
+      <Link to="/register" style={style.item}>
+        <a class="item">Register</a>
+      </Link>
     </div>
   );
 };
