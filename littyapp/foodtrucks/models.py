3167e975c52c721b77f289776d7299ea12717649
Daniel Tian <dtian.95@gmail.com>
2020-05-10 18:34:23 +0800

    create foodtruck model

--- littyapp/foodtrucks/models.py
+++ littyapp/foodtrucks/models.py
@@ -1,10 +1,23 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 
 class FoodTrucks(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100, unique=True)
     address = models.TextField()
     description = models.TextField()
+
+    def __str__(self):
+        return name
+
+
+class FoodItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    foodtruck = models.ForeignKey(FoodTrucks, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- littyapp/littyapp/settings.py
+++ littyapp/littyapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'frontend',
     'accounts',
+    'foodtrucks',
     'rest_framework',
     'knox',
     'django.contrib.admin',
