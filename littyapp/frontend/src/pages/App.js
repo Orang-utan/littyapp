6393006edef59be20b05599c13ac6bdb811ac1d2
Daniel Tian <dtian.95@gmail.com>
2020-05-10 21:41:28 +0800

    added splash screen

--- littyapp/frontend/src/pages/App.js
+++ littyapp/frontend/src/pages/App.js
@@ -8,6 +8,7 @@ import BrokenLink from "./BrokenLink";
 import Navbar from "../components/Navbar";
 import Profile from "./Profile";
 import OrderSuccess from "./OrderSuccess";
+import Splash from "./Splash";
 import TruckMenu from "./TruckMenu";
 import { Container } from "semantic-ui-react";
 import { UserContext } from "../utils/UserContext";
@@ -37,6 +38,7 @@ const App = () => {
             <PrivateRoute exact path="/menu/:id" component={TruckMenu} />
             <PrivateRoute exact path="/profile" component={Profile} />
             <PrivateRoute exact path="/success" component={OrderSuccess} />
+            <PrivateRoute exact path="/splash" component={Splash} />
             <Route path="/" component={BrokenLink} />
           </Switch>
         </UserContext.Provider>
--- littyapp/frontend/src/pages/Splash.js
+++ littyapp/frontend/src/pages/Splash.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Button, Form, Checkbox } from "semantic-ui-react";
 
-const Splash = () => {
+const Splash = ({ history }) => {
+  const handleComplete = () => {
+    history.push("/");
+  };
+
   return (
-    <div>
-      <h1>Splash Screen</h1>
-    </div>
+    <center>
+      <h1 style={{ marginTop: "40px" }}>Welcome to Litty!</h1>
+      <p>
+        Order from our vast selection of food trucks right on the Litty App!
+      </p>
+      <img
+        src="/static/images/order.png"
+        style={{ width: "25%", height: "auto" }}
+      />
+      <div style={{ margin: "30px" }} />
+      <Button primary onClick={handleComplete}>
+        Start Ordering
+      </Button>
+    </center>
   );
 };
 
