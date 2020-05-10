20a3d63ebd9f2f529d66b264fc32725a59617c9f
cinjiangdy <cinjiangdy@users.noreply.github.com>
2020-05-10 05:31:33 -0400

    Update

--- littyapp/text/views.py
+++ littyapp/text/views.py
@@ -5,7 +5,7 @@ from twilio.rest import Client
 
 def sms(request):
     message_to_broadcast = (
-        "Your order has been placed! Come pickup in 15 mins - Littalicious!")
+        "You have an order from Littalicious: ")
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
     for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
         if recipient:
